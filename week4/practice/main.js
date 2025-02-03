a235611c1a496d1241bfb3a0269bfc3ad665203f
Hoa28686 <s2500149@edu.bc.fi>
2025-02-03 14:57:00 +0200

    dom

---
+++ week4/practice/main.js
@@ -0,0 +1,16 @@
+function newFunction() {
+    alert('Some text.')
+} 
+
+newFunction();
+
+function function2() {
+    alert('I told you not to click');
+}
+const circle = document.getElementById('alert');
+console.log(circle.textContent);
+
+const heading = document.getElementsByTagName('h1');
+console.log(heading.innerHTML);
+
+const paragraph = document.getElementsByTagName();
