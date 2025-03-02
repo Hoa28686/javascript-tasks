2b13e1df4e008f57c86a8c936a140fc98ba5e03c
Hoa28686 <195274420+Hoa28686@users.noreply.github.com>
2025-03-03 01:57:41 +0200

    add up to task 12 of object_02.js

--- week6/object/objects_01.js
+++ week6/object/objects_01.js
@@ -3,21 +3,23 @@ Create a JavaScript array named `library` that represents a collection of books.
 Each book should have properties for `title`, `author`, and `yearPublished`.
 Add at least two book objects to this collection.
 */
-let library=[
-    {title:'Atomic Habits',author:'James Clear',yearPublished: 2018},
-    {title:'The Psychology of Money',author:'Morgan Housel',yearPublished: 2020},
-]
-
+let library = [
+  { title: "Atomic Habits", author: "James Clear", yearPublished: 2018 },
+  {
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+    yearPublished: 2020,
+  },
+];
 
 /* Task 2
 Access the `title` of the first book in the `library` collection and log it to the console.
 Modify the `yearPublished` for the second book in the collection to a new year, then log the updated book object to the console.
 */
-console.log('2.');
+console.log("2.");
 console.log(library[0].title);
-library[1].yearPublished=2022;
-console.log(library[1].yearPublished)
-
+library[1].yearPublished = 2022;
+console.log(library[1].yearPublished);
 
 /* Task 3
 Use dot notation to add a new property `genres` (an array of strings) to the first book in the `library`.
@@ -25,9 +27,9 @@ Use bracket notation to add a boolean property `isAvailable` to the same book, i
 */
 
 // Your code here
-console.log('3.');
-library[0].genres=['Self-helf',"Psychology"];
-library[0]['isAvailable']=true;
+console.log("3.");
+library[0].genres = ["Self-helf", "Psychology"];
+library[0]["isAvailable"] = true;
 console.log(library);
 
 /* Task 4
@@ -36,15 +38,19 @@ Using the `Book` constructor, create a new book object with the provided input v
 */
 
 // Your code here
-console.log('4.');
-function Book(title, author, yearPublished,genres){
-    this.title=title;
-    this.author=author;
-    this.yearPublished=yearPublished;
-    this.genres=genres;
-    return{title, author, yearPublished,genres};
+console.log("4.");
+function Book(title, author, yearPublished, genres) {
+  this.title = title;
+  this.author = author;
+  this.yearPublished = yearPublished;
+  this.genres = genres;
+  return { title, author, yearPublished, genres };
 }
-const book3=new Book('The Code Chronicles','Alex J. Harper',2023,["Technology", "Programming", "Self-Improvement"]);
+const book3 = new Book("The Code Chronicles", "Alex J. Harper", 2023, [
+  "Technology",
+  "Programming",
+  "Self-Improvement",
+]);
 library.push(book3);
 console.log(library);
 /* Task 5
@@ -52,27 +58,32 @@ Write a function named `createBook` that takes parameters for `title`, `author`,
 The function should return a new book object with these properties.
 Test `createBook` by creating a new book object with user-provided input and logging the new book object to the console.
 */
-console.log('5.');
-function createBook(title, author, yearPublished,genres){
-    return{
-        title:title,
-        author:author,
-        yearPublished:yearPublished,
-        genres:genres
-    };
+console.log("5.");
+function createBook(title, author, yearPublished, genres) {
+  return {
+    title: title,
+    author: author,
+    yearPublished: yearPublished,
+    genres: genres,
+  };
 }
 
-const book4 = createBook('All the Light We Cannot See','Anthony Doerr',2021,["Historical Fiction", "War Fiction", "Literary Fiction", "Coming-of-Age"]);
-console.log(book4)
+const book4 = createBook("All the Light We Cannot See", "Anthony Doerr", 2021, [
+  "Historical Fiction",
+  "War Fiction",
+  "Literary Fiction",
+  "Coming-of-Age",
+]);
+console.log(book4);
 /* Task 6
 Convert the `library` collection to a JSON string and log it to the console.
 Parse the JSON string back into a JavaScript object and log the first book's title to the console.
 */
-console.log('6.');
+console.log("6.");
 library = JSON.stringify(library);
 console.log(library);
 
-library=JSON.parse(library);
+library = JSON.parse(library);
 console.log(library);
 /* Task 7
 Create an array named `students`, where each student has a `name`, `age`, and `grade`.
@@ -80,67 +91,67 @@ Log the name of any student who has a grade higher than 90.
 */
 
 // Your code here
-console.log('7. Name of student who has a grade higher than 90:');
+console.log("7. Name of student who has a grade higher than 90:");
 const students = [
-    { name: "Alice", age: 18, grade: 90 },
-    { name: "Bob", age: 19, grade: 98 },
-    { name: "Charlie", age: 17, grade: 70}
-  ];
-students.forEach(s=>{
-    if (s.grade>90){
-        console.log(s.name);
-    }
-})  
+  { name: "Miria", age: 18, grade: 90 },
+  { name: "Ran", age: 19, grade: 98 },
+  { name: "Sara", age: 17, grade: 70 },
+];
+students.forEach((s) => {
+  if (s.grade > 90) {
+    console.log(s.name);
+  }
+});
 /* Task 8
 Define an object named `car` with properties for `brand`, `model`, `year`, and `isElectric`.
 Use a function to check if the car is electric. If it is, log `"This car is eco-friendly!"`. Otherwise, log `"This car runs on fuel."`
 */
-console.log('8.');
+console.log("8.");
 const car = {
-    brand: "Tesla",
-    model: "Model S",
-    year: 2022,
-    isElectric: true
-  };
+  brand: "Tesla",
+  model: "Model S",
+  year: 2022,
+  isElectric: true,
+};
 
-  function checkCar(car){
-    if (car.isElectric){
-        console.log('This car is eco-friendly!');
-    }else{
-        console.log("This car runs on fuel.");
-    }
+function checkCar(car) {
+  if (car.isElectric) {
+    console.log("This car is eco-friendly!");
+  } else {
+    console.log("This car runs on fuel.");
   }
-  checkCar(car);
+}
+checkCar(car);
 
 /* Task 9
 Create a `movies` array where each movie is an object with properties for `title`, `director`, and `rating`.
 Write a loop that logs all movie titles with a rating higher than 8.
 */
-console.log('9.');
+console.log("9.");
 const movies = [
-    { title: "Inception", director: "Christopher Nolan", rating: 8.8 },
-    { title: "The Godfather", director: "Francis Ford Coppola", rating: 9.2 },
-    { title: "Interstellar", director: "Christopher Nolan", rating: 8 }
-  ];
-movies.forEach(m=>{
-    if (m.rating>8){
-        console.log(m.title);
-    }
-})
+  { title: "Inception", director: "Christopher Nolan", rating: 8.8 },
+  { title: "The Godfather", director: "Francis Ford Coppola", rating: 9.2 },
+  { title: "Interstellar", director: "Christopher Nolan", rating: 8 },
+];
+movies.forEach((m) => {
+  if (m.rating > 8) {
+    console.log(m.title);
+  }
+});
 /* Task 10
 Write a function `findOldestCar` that takes an array of car objects and returns the car with the earliest `year`.
 */
-console.log('10.');
+console.log("10.");
 const cars = [
-    { brand: "Tesla", model: "Model S", year: 2022, isElectric: true },
-    { brand: "Toyota", model: "Corolla", year: 2020, isElectric: false },
-    { brand: "Ford", model: "Mustang Mach-E", year: 2021, isElectric: true },
-    { brand: "BMW", model: "X5", year: 2019, isElectric: false },
-    { brand: "Audi", model: "e-tron", year: 2023, isElectric: true }
-  ];
-function findOldestCar(cars){
-    cars.sort((c1,c2)=>c1.year-c2.year);
-    return cars[0];
+  { brand: "Tesla", model: "Model S", year: 2022, isElectric: true },
+  { brand: "Toyota", model: "Corolla", year: 2020, isElectric: false },
+  { brand: "Ford", model: "Mustang Mach-E", year: 2021, isElectric: true },
+  { brand: "BMW", model: "X5", year: 2019, isElectric: false },
+  { brand: "Audi", model: "e-tron", year: 2023, isElectric: true },
+];
+function findOldestCar(cars) {
+  cars.sort((c1, c2) => c1.year - c2.year);
+  return cars[0];
 }
 
 console.log(findOldestCar(cars));
@@ -149,31 +160,46 @@ Define a `userProfiles` array containing objects representing users.
 Each user should have a `username`, `email`, and `isAdmin` property.
 Write a function that filters and returns only the users who are administrators.
 */
-console.log('11.');
+console.log("11.");
 const userProfiles = [
-    { username: "john_doe", email: "john@example.com", isAdmin: false },
-    { username: "admin_user", email: "admin@example.com", isAdmin: true },
-    { username: "jane_smith", email: "jane@example.com", isAdmin: false }
-  ];
-function adminCheck(){
-    const admin=userProfiles.filter(u=>(u.isAdmin));
-    return admin;
-}  
+  { username: "ran_mouri", email: "ran@gmail.com", isAdmin: false },
+  { username: "admin_user", email: "admin@gamil.com", isAdmin: true },
+  { username: "black_smith", email: "smith@gamil.com", isAdmin: false },
+];
+function adminCheck() {
+  const admin = userProfiles.filter((u) => u.isAdmin);
+  return admin;
+}
 console.log(adminCheck());
 /* Task 12
 Define an array named `orders`, where each order contains `orderId`, `customerName`, `totalAmount`, and `status` (either "pending" or "completed").
 Write a function that returns only the completed orders.
 */
-console.log('12.');
+console.log("12.");
 const orders = [
-    { orderId: 101, customerName: "Alice Johnson", totalAmount: 250.75, status: "completed" },
-    { orderId: 102, customerName: "Bob Smith", totalAmount: 89.99, status: "pending" },
-    { orderId: 103, customerName: "Charlie Brown", totalAmount: 150.50, status: "completed" },
-    { orderId: 104, customerName: "David Lee", totalAmount: 300.00, status: "pending" }
-  ];
-function complete(orders){
-    return orders.filter(o=>o.status=='completed');
-}  
+  {
+    orderId: 101,
+    customerName: "Ran",
+    totalAmount: 250.75,
+    status: "completed",
+  },
+  {
+    orderId: 102,
+    customerName: "Donald",
+    totalAmount: 89.99,
+    status: "pending",
+  },
+  {
+    orderId: 103,
+    customerName: "Princess",
+    totalAmount: 150.5,
+    status: "completed",
+  },
+  { orderId: 104, customerName: "Park", totalAmount: 300.0, status: "pending" },
+];
+function complete(orders) {
+  return orders.filter((o) => o.status == "completed");
+}
 console.log(complete(orders));
 
 /* Task 13
@@ -182,47 +208,51 @@ Write a function that logs `"This phone supports 5G!"` if `is5GEnabled` is `true
 */
 
 // Your code here
-console.log('13.');
+console.log("13.");
 const smartphone = {
-    brand: "Samsung",
-    model: "Galaxy S23",
-    batteryLife: "24 hours",
-    is5GEnabled: true
-  };
-  const check5G=smartphone=>{
-    (smartphone.is5GEnabled)?console.log('This phone supports 5G.'):console.log('This phone does notsupport 5G.')
-  }
-  check5G(smartphone);
+  brand: "Samsung",
+  model: "Galaxy S23",
+  batteryLife: "24 hours",
+  is5GEnabled: true,
+};
+const check5G = (smartphone) => {
+  smartphone.is5GEnabled
+    ? console.log("This phone supports 5G.")
+    : console.log("This phone does notsupport 5G.");
+};
+check5G(smartphone);
 /* Task 14
 Define an object named `fox` with properties `name`, `age`, and `habitat`.
 Write a function that logs `"This fox is young"` if its age is under 3, `"This fox is an adult"` if 3 or older.
 */
-console.log('14.');
+console.log("14.");
 const fox = {
-    name: "Red Fox",
-    age: 3,
-    habitat: "Forests and grasslands"
-  };
- 
-const checkAge=fox=>{
-    (fox.age<3)?console.log('This fox is young'):console.log('This fox is an adult');
-}
+  name: "Red Fox",
+  age: 3,
+  habitat: "Forests and grasslands",
+};
+
+const checkAge = (fox) => {
+  fox.age < 3
+    ? console.log("This fox is young")
+    : console.log("This fox is an adult");
+};
 checkAge(fox);
 /* Task 15
 Define an array named `employees`, where each employee has `name`, `position`, and `salary`.
 Write a function that calculates the total salary of all employees in the company.
 */
 
-console.log('15.');
+console.log("15.");
 const employees = [
-    { name: "Alice Johnson", position: "Software Engineer", salary: 80000 },
-    { name: "Bob Smith", position: "Project Manager", salary: 95000 },
-    { name: "Charlie Brown", position: "UI/UX Designer", salary: 70000 },
-    { name: "David Lee", position: "DevOps Engineer", salary: 85000 }
-  ];
-  function totalSalary(){
-    let sum=0;
-    employees.forEach(e=> sum+=e.salary);
-    return sum;
-  }
+  { name: "Ran", position: "Software Engineer", salary: 80000 },
+  { name: "Charlie", position: "Project Manager", salary: 95000 },
+  { name: "Shinichi", position: "UI/UX Designer", salary: 70000 },
+  { name: "Suneo", position: "DevOps Engineer", salary: 85000 },
+];
+function totalSalary() {
+  let sum = 0;
+  employees.forEach((e) => (sum += e.salary));
+  return sum;
+}
 console.log(totalSalary());
--- week6/object/objects_02.js
+++ week6/object/objects_02.js
@@ -1,113 +1,203 @@
-/* Task 1
-Create an object `weather` that contains properties `temperature`, `humidity`, and `condition` (e.g., "Sunny", "Rainy").
-Write a function that logs `"Take an umbrella!"` if it's rainy.
-*/
-const weather={temperature: 25, humidity:50,condition:'rainy'
+// task1:
+
+console.log("1.");
+const weather = { temperature: 25, humidity: 50, condition: "rainy" };
+
+const weatherCheck = (weather) => {
+  if (weather.condition == "rainy") {
+    console.log("Take an umbrella!");
+  }
+};
+weatherCheck(weather);
+
+// task2:
+console.log("2. Total price of all items in the cart:");
+const shoppingCart = {
+  items: [
+    { name: "fish", price: 6.7, quantity: 2 },
+    { name: "carrot", price: 0.99, quantity: 1 },
+    { name: "kale", price: 3.5, quantity: 5 },
+    { name: "orange", price: 4.2, quantity: 3 },
+    { name: "rice", price: 1.99, quantity: 1 },
+  ],
+};
+
+function totalPrice() {
+  const sum = shoppingCart.items.reduce(
+    (sum, i) => sum + i.price * i.quantity,
+    0
+  );
+  return Number(sum.toFixed(2));
 }
-const weatherCheck=(weather)=>{
-    if(weather.condition=='rainy'){
-        console.log("Take an umbrella!");
+console.log(totalPrice());
+// need to have () when calling totalPrice because it's a function. not a variable
+
+// task 3
+console.log("3. Restaurants with a rating of 4 or higher:");
+const restaurants = [
+  { name: "PizzaInn", cuisineType: "Italian", rating: 3.7 },
+  { name: "Bao Dimsum", cuisineType: "Cantonese", rating: 4.5 },
+  { name: "Pho Viet", cuisineType: "Vietnamese", rating: 4.3 },
+  { name: "Haidilao", cuisineType: "Chinese", rating: 4.0 },
+  { name: "Lotteria", cuisineType: "Fast Food", rating: 4.1 },
+];
+const highRating = () => {
+  return restaurants.filter((r) => r.rating >= 4);
+};
+console.log(highRating());
+
+// task 4
+console.log("4. Total balance after all transactions: ");
+const bankAccount = {
+  accountHolder: "Harry Potter",
+  balance: 1250.6,
+  transactions: [200, 20, -15, -2, 35],
+};
+const balance = () => {
+  return bankAccount.transactions.reduce(
+    (sum, t) => sum + t,
+    bankAccount.balance
+  );
+};
+
+console.log(balance());
+
+// task 5
+console.log("5. Calculates and updates each student's `averageScore`:");
+const students = [
+  { name: "Mikey", scores: [3, 5.6, 8, 7.5], averageScore: null },
+  { name: "Donald", scores: [4, 6, 8.7, 9], averageScore: null },
+  { name: "Tom", scores: [5, 5.3, 7.9, 2], averageScore: null },
+  { name: "Jerry", scores: [3.5, 7.4, 6.8, 9.5], averageScore: null },
+];
+const average = () => {
+  students.forEach((student) => {
+    const avg =
+      student.scores.reduce((sum, score) => sum + score, 0) /
+      student.scores.length;
+    student.averageScore = Number(avg.toFixed(2));
+  });
+};
+average();
+console.log(students);
+
+// task 6
+console.log("6.");
+const courses = [
+  { courseName: "Javascript", instructor: "Nobita", studentsEnrolled: 20 },
+  { courseName: "Python", instructor: "Doraemon", studentsEnrolled: 25 },
+  { courseName: "HTML", instructor: "Shizuka", studentsEnrolled: 30 },
+  { courseName: "UI design", instructor: "Suneo", studentsEnrolled: 35 },
+  { courseName: "Singing", instructor: "Gian", studentsEnrolled: 40 },
+  { courseName: "Laravel", instructor: "Dekisugi", studentsEnrolled: 51 },
+];
+
+const more30 = (courses) => {
+  const Arr = [];
+  courses.forEach((c) => {
+    if (c.studentsEnrolled > 30) {
+      Arr.push(c.courseName);
     }
-}
-weather(weather);
+  });
+  console.log(`Courses that have more than 30 students: ${Arr.join(", ")}.`);
+};
+more30(courses);
+
+// task 7
+console.log("7.");
+
+const pet = { species: "Dog", name: "Steam Buns", isVaccinated: true };
+
+const checkVaccine = () => {
+  if (!pet.isVaccinated) {
+    return "Vaccination required";
+  } else {
+    return "No vaccination needed";
+  }
+};
+console.log(checkVaccine());
+// task 8
+console.log("8.");
+const city = {
+  name: "Ho Chi Minh city",
+  population: 8900000,
+  landmark: "Saigon Opera House",
+};
+const checkPopulation = () => {
+  city.population > 1000000
+    ? console.log("This is a big city!")
+    : console.log("");
+};
+checkPopulation();
+
+// task 9
+console.log("9.");
+const transactions = [
+  { type: "credit", amount: 30 },
+  { type: "credit", amount: 300 },
+  { type: "dedit", amount: 230 },
+  { type: "credit", amount: 350 },
+  { type: "dedit", amount: 706 },
+];
+
+const totalBalance = () => {
+  return transactions.reduce((sum, t) => sum + t.amount, 0);
+};
+console.log(`Total balance: ${totalBalance()}`);
+
+// task 10
+console.log("10.");
+const foxPack = {
+  foxes: [
+    { name: "Ran", age: 3, furColor: "orange" },
+    { name: "Conan", age: 2, furColor: "blue" },
+    { name: "Mouri", age: 1, furColor: "brown" },
+    { name: "Haibara", age: 1, furColor: "yellow" },
+    { name: "Kaito", age: 4, furColor: "white" },
+  ],
+};
+const youngFox = () => {
+  return foxPack.foxes.filter((f) => f.age < 2);
+};
+
+console.log(youngFox());
+
+// task 11
+console.log("11. All items in the character’s inventory:");
+const gameCharacter = {
+  name: "Princess",
+  level: 15,
+  health: 200,
+  inventory: ["shield", "wand", "broomstick", "invisibility cloak"],
+};
+const int = () => {
+  console.log(gameCharacter.inventory.join(", "));
+};
+int();
+
+// task 12
+console.log("12.");
+const employees = [
+  { name: "Harry", role: "Front-end developer", workingHours: 15 },
+  { name: "Hermione", role: "Software developer", workingHours: 47 },
+  { name: "Ron", role: "Project manager", workingHours: 38 },
+  { name: "Draco", role: "Graphic designer", workingHours: 45 },
+];
+const work40 = () => {
+  const workAlot = employees.filter((e) => e.workingHours > 40);
+  console.log(`Employees who work more than 40 hours:`);
+  workAlot.forEach((e) => {
+    console.log(
+      `- Name: ${e.name}, role: ${e.role}, works ${e.workingHours} hours.`
+    );
+  });
+};
+work40();
 
 
-/* Task 2
-Define an object named `shoppingCart` that contains an array of items.
-Each item has `name`, `price`, and `quantity`.
-Write a function that calculates the total price of all items in the cart.
-*/
-
-// Your code here
-
-/* Task 3
-Create an array of objects named `restaurants`, each with properties `name`, `cuisineType`, and `rating`.
-Write a function that returns only the restaurants with a rating of 4 or higher.
-*/
-
-// Your code here
-
-/* Task 4
-Define an object named `bankAccount` with properties `accountHolder`, `balance`, and `transactions` (an array of numbers).
-Write a function that calculates the total balance after all transactions.
-*/
-
-// Your code here
-
-/* Task 5
-Create an array `students` where each student has `name`, `scores` (array of numbers), and `averageScore` (initially null).
-Write a function that calculates and updates each student's `averageScore`.
-*/
-
-// Your code here
-
-/* Task 6
-Define an array named `courses`, each containing `courseName`, `instructor`, and `studentsEnrolled`.
-Write a function that logs the course name of any course with more than 30 students.
-*/
-
-// Your code here
-
-/* Task 7
-Create an object `pet` with properties `species`, `name`, and `isVaccinated`.
-Write a function that returns `"Vaccination required"` if the pet is not vaccinated.
-*/
-
-// Your code here
-
-/* Task 8
-Define an object named `city` with properties `name`, `population`, and `landmark`.
-Write a function that logs `"This is a big city!"` if the population exceeds 1,000,000.
-*/
-
-// Your code here
-
-/* Task 9
-Create an array `transactions`, where each transaction is an object with `type` ("credit" or "debit") and `amount`.
-Write a function that calculates the total balance after processing all transactions.
-*/
-
-// Your code here
-
-/* Task 10
-Define an object named `foxPack` containing an array of fox objects.
-Each fox has `name`, `age`, and `furColor`.
-Write a function that filters out only the foxes that are younger than 2.
-*/
-
-// Your code here
-
-/* Task 11
-Create an object `gameCharacter` with properties `name`, `level`, `health`, and `inventory` (an array).
-Write a function that logs all items in the character’s inventory.
-*/
-
-// Your code here
-
-/* Task 12
-Define an array named `employees`, where each employee has a `name`, `role`, and `workingHours`.
-Write a function that finds and logs employees who work more than 40 hours.
-*/
-
-// Your code here
-
-/* Task 13
-Create an array `musicAlbums` where each album has `title`, `artist`, and `releaseYear`.
-Write a function that logs albums released after 2000.
-*/
-
-// Your code here
-
-/* Task 14
-Define an array named `cars`, each containing `brand`, `model`, and `horsepower`.
-Write a function that finds and returns the car with the highest horsepower.
-*/
-
-// Your code here
-
-/* Task 15
-Create an array of `airports`, where each airport has `name`, `country`, and `flightsPerDay`.
-Write a function that finds the airport with the most daily flights.
-*/
-
-// Your code here
+// task 13
+console.log("13.");
+// task 14
+console.log("14.");
+// task 15
+console.log("15.");
