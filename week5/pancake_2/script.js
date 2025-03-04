d6189101e9c3848f400d39566c1b7294bde0a5b6
Hoa28686 <195274420+Hoa28686@users.noreply.github.com>
2025-03-04 02:40:20 +0200

    add pancake2

--- week5/pancake_2/script.js
+++ week5/pancake_2/script.js
@@ -1,49 +1,129 @@
-const type=document.querySelector('#type');
-const totalPrice=document.querySelector('#totalPrice');
-const totalDisplay=document.querySelector('#totalPriceDisplay');
-const pancake_form = document.querySelector('#pancakeForm');
-const seeBtn = document.querySelector('#seeOrder');
-const summary= document.querySelector('#orderSummary.p');
+const bannerPrice = document.querySelector("#totalPrice");
+const totalDisplay = document.querySelector("#totalPriceDisplay");
+const seeBtn = document.querySelector("#seeOrder");
+const summaryText = document.querySelector("#summaryText");
+const customerName = document.querySelector("#customerName");
+let sum1 = 5;
+let sum2 = 0;
+let sum3 = 0;
+let sum4 = 0;
 
-let sum1=5;
-let sum2=0;
-function update_sum(){
-    totalPrice.textContent=totalDisplay.textContent = sum1+sum2+"€";
-    console.log(sum1+sum2);
+pancakeForm.addEventListener("change", (e) => {
+  pancakeType(e);
+  //   ingredient(e);
+  topping(e);
+  extra(e);
+  delivery(e);
+});
+seeBtn.addEventListener("click", summary);
+
+function update_sum() {
+  bannerPrice.textContent = totalDisplay.textContent =
+    sum1 + sum2 + sum3 + sum4 + "€";
 }
 
-const print=['Classic - 5€'];
-const array=[{name:'',value:''}];
-pancake_form.addEventListener('change', (event)=>{
-    let target=event.target
-    if (target.id==="type"){
-        sum1=Number(type.selectedOptions[0].dataset.price);
-        print[0]= type.selectedOptions[0].textContent;
-    }
-    
-    if ((target.classList.contains('topping')) || (target.classList.contains('extra'))){
-        const price= target.dataset.price;
-        const text =target.parentNode.textContent.replace(/\s+/g,' ');
-        if (target.checked){
-            array.push({name:text, value:price});
-        } else {
-            const index= array.findIndex(i => i.name==text)
-            array.splice(index,1);
-        } 
-        sum2=array.reduce((sum2,i)=> sum2+Number(i.value),0);
-        
-        
-        
-    }
-    
+// Select pancake type
+let type = "Classsic 5€";
+function pancakeType(e) {
+  if (e.target.id == "type") {
+    sum1 = Number(
+      e.target.options[e.target.selectedIndex].getAttribute("data-price")
+    );
+    type = e.target.options[e.target.selectedIndex].textContent;
     update_sum();
+  }
+}
 
-   
-   
-})
+// //template for topping and extra items
+function Arr(name, price) {
+  this.name = name;
+  this.price = price;
+}
 
+let toppingArr = [];
+let extraArr = [];
 
+// //topping function
+function topping(e) {
+  //console.log(e.target.parentElement);
+  //console.log(e.target);
+  if (e.target.className == "topping") {
+    const toppingName = e.target.parentElement.textContent.trim();
+    const toppingPrice = Number(e.target.getAttribute("data-price"));
+    const top = new Arr(toppingName, toppingPrice);
+    if (e.target.checked) {
+      toppingArr.push(top);
+    } else {
+      toppingArr = toppingArr.filter((t) => t.name !== toppingName);
+    }
+    sum2 = toppingArr.reduce((sum, t) => sum + t.price, 0);
+    update_sum();
+  }
+}
 
-// seeBtn.addEventListener('click',()=>{
-//     summary.appendChild()
-// })
+//extra function
+function extra(e) {
+  if (e.target.className == "extra") {
+    const extraName = e.target.parentElement.textContent.trim();
+    const extraPrice = Number(e.target.getAttribute("data-price"));
+    const extra = new Arr(extraName, extraPrice);
+    if (e.target.checked) {
+      extraArr.push(extra);
+    } else {
+      extraArr = extraArr.filter((e) => e.name !== extraName);
+    }
+    sum3 = extraArr.reduce((sum, e) => sum + e.price, 0);
+    update_sum();
+  }
+}
+
+//delivery method
+let deli = new Arr("Eat In", 0);
+function delivery(e) {
+  if (e.target.className == "delivery") {
+    const deliName = e.target.parentElement.textContent.trim();
+    const deliPrice = Number(e.target.getAttribute("data-price"));
+    deli = new Arr(deliName, deliPrice);
+    sum4 = deliPrice;
+    update_sum();
+  }
+}
+
+//summarize order
+function summary() {
+  const summary = `
+  Customer name:${customerName.value.trim()}<br>
+  Pancake type: ${type}<br>
+  Topping: ${toppingArr.map((t) => t.name).join(", ")} ${sum2}€<br>
+  Extra: ${extraArr.map((e) => e.name).join(", ")} <br>
+  ${deli.name}`;
+  summaryText.innerHTML = summary;
+}
+
+// Method2: if task doesn't require two seperate arrays for topping and extra items -> combine topping and extra function together (comment to hide topping and extra in addEventListener, topping, extra, summary functions above, uncomment line 13 and from 103 forward)
+
+// let items = [];
+// function ingredient(e) {
+//   if (e.target.className == "topping" || e.target.className == "extra") {
+// const iName = e.target.parentElement.textContent.trim();
+// const iPrice = Number(e.target.getAttribute("data-price"));
+// const item = new Arr(iName, iPrice);
+// if (e.target.checked) {
+//   items.push(item);
+// } else {
+//   items = items.filter((i) => i.name !== iName);
+// }
+// sum2 = items.reduce((sum, i) => sum + i.price, 0);
+// update_sum();
+//   }
+// }
+//
+// function summary() {
+//   const summary = `
+// Customer name:${customerName.value.trim()}<br>
+// Pancake type: ${type}<br>
+// Topping and extra ${sum2}€: ${items.map((i) => i.name).join(", ")} <br>
+// ${deli.name}`;
+//   summaryText.innerHTML = summary;
+// }
+//
